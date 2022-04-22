8a13957de89b7c957b210b229a5b8810e9bdaf1d
Grégori Sabel <seriosabel57@gmail.com>
2022-04-22 09:39:17 -0300

    floor to ceil

--- src/components/Pagination/Index.tsx
+++ src/components/Pagination/Index.tsx
@@ -24,7 +24,7 @@ export default function Pagination( {
   currentPage = 1,
   onPageChange,
  }: PaginationProps ) {  
-  const lastPage = Math.floor(totalCountOfRegisters / registersPerPage);
+  const lastPage = Math.ceil(totalCountOfRegisters / registersPerPage);
 
 
   const previousPages = currentPage > 1 
